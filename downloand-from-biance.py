01e72b439ad71a3d4eafdf039db84cc5026174ae
Rohit-9862 <162949904+Rohit-9862@users.noreply.github.com>
2025-03-28 06:36:16 +0000

    downloaded data

--- downloand-from-biance.py
+++ downloand-from-biance.py
@@ -7,16 +7,20 @@ import os
 binance = ccxt.binance()
 
 # Define multiple trading pairs
-symbols = [""]#["BTC/USDT", "ETH/USDT", "BNB/USDT"]  # Add more as needed
+symbols = ["BTC/USDT", "ETH/USDT", "BNB/USDT",
+'PAXG/USDT', 'FTT/USDT', 'REQ/USDT', 'CETUS/USDT', 'AAVE/USDT', 'PROM/USDT', 'TAO/USDT', 'ACA/USDT', 'COOKIE/USDT', 'TRX/USDT', 'G/USDT', 'BAR/USDT', 'DEXE/USDT', 'TST/USDT', 'BICO/USDT', 'SHELL/USDT', 'ARKM/USDT', 'SUN/USDT', 'CRV/USDT', 'JST/USDT', 'EUR/USDT', 'AR/USDT', 'EURI/USDT', 'HMSTR/USDT', 'FDUSD/USDT', 'SYS/USDT', 'DGB/USDT', 'SC/USDT', 'USDC/USDT', 'TUSD/USDT', 'USDP/USDT', 'AEUR/USDT', 'ENS/USDT', 'TFUEL/USDT', 'BNT/USDT', 'BEL/USDT', 'JUV/USDT', 'HOOK/USDT', 'KAIA/USDT', 'VTHO/USDT']
 timeframe = "1h"
 
 # Define date range
-since = binance.parse8601('2024-01-01T00:00:00Z')  # Start date
-until = binance.parse8601('2025-03-01T00:00:00Z')  # End date
-
+#since = binance.parse8601('2024-01-01T00:00:00Z')  # Start date
+#until = binance.parse8601('2025-03-01T00:00:00Z')  # End date
+since = binance.parse8601('2025-03-01T00:00:00Z')
+until =binance.parse8601('2025-03-25T00:00:00Z')
 # Create directory to save data
-output_dir = "2024-01-01-to-2025-03-01"
+#output_dir = "2024-01-01-to-2025-03-01"
+output_dir ="2025-03-01-to-2025-03-25"
 os.makedirs(output_dir, exist_ok=True)
+#os.makedirs(output, exist_ok=True)
 
 # Loop through each symbol and download data
 for symbol in symbols:
